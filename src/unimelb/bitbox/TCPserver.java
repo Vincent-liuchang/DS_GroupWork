becc6a0f68d2657c96e7cff7bc4c3a7282828823
yw <ywang035@gmail.com>
2019-06-02 04:07:08 +1000

    Merge branch 'master' of https://github.com/Vincent-liuchang/DS_GroupWork
    # Conflicts:
    #	configuration.properties

--- src/unimelb/bitbox/TCPserver.java
+++ src/unimelb/bitbox/TCPserver.java
@@ -45,21 +45,6 @@ public class TCPserver extends Thread{
 
 				Socket client = server.accept();
 				counter++;
-				if(Socketlist.size() != 0) {
-					for (int i = 0; i< Socketlist.size(); i++) {
-						if (Socketlist.get(i).isClosed()) {
-							HostPort h = new HostPort(Socketlist.get(i).getInetAddress().toString().split("/")[1],Socketlist.get(i).getPort());
-							serverlist.remove(h);
-							peer.peerHosts.remove(h);
-							for(TCPclient t: peer.clientList){
-								if(t.hostport.equals(h)){
-									t.interrupt();
-								}
-							}
-							Socketlist.remove(i);
-						}
-					}
-				}
 
 				Socketlist.add(client);
 				System.out.println("Now the server has " + Socketlist.size() + " clients"+"\n");
@@ -164,6 +149,21 @@ public class TCPserver extends Thread{
 			System.out.println("1 client off line");
 			System.out.println("onlinepeer list need to be update:");
 			System.out.println(Socketlist.toString());
+			if(Socketlist.size() != 0) {
+				for (int i = 0; i< Socketlist.size(); i++) {
+					if (Socketlist.get(i).isClosed()) {
+						HostPort h = new HostPort(Socketlist.get(i).getInetAddress().toString().split("/")[1],Socketlist.get(i).getPort());
+						serverlist.remove(h);
+						peer.peerHosts.remove(h);
+						for(TCPclient t: peer.clientList){
+							if(t.hostport.equals(h)){
+								t.interrupt();
+							}
+						}
+						Socketlist.remove(i);
+					}
+				}
+			}
 
 		} catch (IOException | NoSuchAlgorithmException e) {
 			e.printStackTrace();
